85209dba5b60e2c046a51325925113d8529522f7
Adam76e54 <gillesad@tcd.ie>
2026-05-09 22:57:26 +0100

    Messed around with how the pid controller works syntactically in rust

--- drone_firmware/flight/src/main.rs
+++ drone_firmware/flight/src/main.rs
@@ -2,7 +2,10 @@ use control::pid::{PidCoefficients, PidController, PidLimiters};
 fn main() {
     println!("Hello, world!");
     let coefficients = PidCoefficients {kp: 0.0, ki: 0.0, kd: 0.0, kff: 0.0};
-    let limiters: PidLimiters = PidLimiters { integral_limit: 0.0, error_filter_frequency_hz: 0.0, derviative_filter_frequency_hz: 0.0 }
+    let limiters: PidLimiters = PidLimiters { 
+        integral_limit: 0.0, 
+        error_filter_frequency_hz: 0.0, 
+        derviative_filter_frequency_hz: 0.0 };
     let mut roll_pid = PidController::new(coefficients, limiters);
-    
+
 }
