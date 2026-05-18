0f9b8c115d54680a96c8b8cb4b03909967ebc83d
Adam76e54 <gillesad@tcd.ie>
2026-05-18 23:09:02 +0100

    hid flashing script, it's dependent on local filsystem organisation anyway

---
+++ drone_firmware/flash.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+cargo build -p flight --release
+
+$projectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+$buildElf    = Join-Path $projectRoot "target\thumbv7em-none-eabihf\release\flight"
+$flashElf    = Join-Path $projectRoot "target\thumbv7em-none-eabihf\release\flight.elf"
+$cubeProg    = "C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe"
+
+if (-not (Test-Path $buildElf)) {
+    throw "Built ELF not found at: $buildElf"
+}
+
+if (Test-Path $flashElf) {
+    Remove-Item $flashElf -Force
+}
+
+Copy-Item $buildElf $flashElf -Force
+
+& $cubeProg `
+  --connect port=usb1 `
+  --download $flashElf `
+  --start 0x08000000
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Flashing failed with exit code $LASTEXITCODE"
+}
